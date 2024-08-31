e1cf1ebe94f44e74bf2c0d94051dfdfca6b781b7
hawat <nuri@192.168.1.75>
2024-08-31 16:26:40 +0300

    Updates

--- src/Author.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Author = ({ author }) => (
-  <p id="author">- {author}</p>
-);
-
-export default Author;
--- src/Quote.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Quote = ({ text }) => (
-  <p id="text" className="quote-text mb-4 font-italic">“{text}”</p>
-);
-
-export default Quote;
